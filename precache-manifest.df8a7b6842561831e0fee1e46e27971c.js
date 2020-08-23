5d70f56cd5444450fca3f3047b541960094ef2e2
ShaharAF <shahar.cohen@appsflyer.com>
2020-08-23 11:58:26 +0300

    Updates

---
+++ precache-manifest.df8a7b6842561831e0fee1e46e27971c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8e1b3e2d0579e225e810c19c978daf",
+    "url": "/projects-page/index.html"
+  },
+  {
+    "revision": "2ed80bbc8e95b6011790",
+    "url": "/projects-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c7e64fe8252d2b5f53c6",
+    "url": "/projects-page/static/js/2.af8da06d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projects-page/static/js/2.af8da06d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed80bbc8e95b6011790",
+    "url": "/projects-page/static/js/main.cbff934d.chunk.js"
+  },
+  {
+    "revision": "aab4aa60e2c600ac2579",
+    "url": "/projects-page/static/js/runtime-main.094aaaa9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/projects-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
